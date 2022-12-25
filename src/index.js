9ddbd4d2b81c79ae97ab5568ee51526f89ad165c
95275566 <43737469+95275566@users.noreply.github.com>
2022-12-26 01:29:27 +0800

    commit time work

--- src/index.js
+++ src/index.js
@@ -6,15 +6,18 @@ import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <div>
-      <App />
-    </div>
-  </React.StrictMode>
-);
+function tick() {
+    root.render(
+        <React.StrictMode>
+            <div>
+                <App />
+            </div>
+        </React.StrictMode>
+    );
+}
 
-setInterval(App, 1000);
+
+setInterval(tick, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
