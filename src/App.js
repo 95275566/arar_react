65c18e8dec3e6377ee56228866d131d202542fc7
95275566 <43737469+95275566@users.noreply.github.com>
2022-12-25 23:42:47 +0800

    message

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+const button =()=>{
+  var output=[];
+    for(let i=0;i<4;++i)
+        output.push(<button>我是第{i}個按鍵</button>)
+    return output;
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>It is {new Date().toLocaleTimeString()}.</p>
+        <br />
+        <p>
+          Edit <code>src/App.js</code> and save to reload 12345125.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        { button() }
+      </header>
+    </div>
+  );
+}
+
+export default App;
